c0250c289261774a2bd05d24c8a0f985839d7e05
moonhr <moonhr153@gmail.com>
2024-12-06 11:05:08 +0900

    test 컴파일

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// src/index.ts
+const greet = (name) => {
+    return `Hello, ${name}! Welcome to Turbopack with TypeScript.`;
+};
+console.log(greet("Developer"));
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// src/index.ts
+
+const greet = (name: string): string => {
+  return `Hello, ${name}! Welcome to Turbopack with TypeScript.`;
+};
+
+console.log(greet("Developer"));
